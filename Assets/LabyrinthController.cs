ebae7fb691a491b8df659f921871c90d1cd009d9
Oliver Stadie <hello@waog.net>
2017-11-16 20:23:35 +0100

    ADD script to add rooms in scene to script list("array")

---
+++ Assets/LabyrinthController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LabyrinthController : MonoBehaviour {
+
+	public List<List<GameObject>> rows;
+
+	// Use this for initialization
+	void Start () {
+		rows = new List<List<GameObject>> ();
+			
+		foreach (GameObject rowGO in GetChildrenByTag (gameObject, "row"))
+		{
+			List<GameObject> rooms = GetChildrenByTag (rowGO, "room");
+			rows.Add (rooms);
+		}
+
+		Debug.Log (rows.Count);
+		Debug.Log (rows[0].Count);
+		Debug.Log (rows[3].Count);
+	}
+
+	void Update () {
+	
+	}
+
+	List<GameObject> GetChildrenByTag(GameObject parent, string tag) {
+		List<GameObject> result = new List<GameObject> ();
+
+		for (int i = 0; i < parent.transform.childCount; i++) {
+			GameObject child = parent.transform.GetChild (i).gameObject;
+			if (child.CompareTag(tag)) {
+				result.Add(child);
+			}
+		}
+		return result;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (3f, 0, 0);
+		}
+	}
+}
